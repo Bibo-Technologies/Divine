563140e46f04575f950a35b20e4b273a07c7ab81
Bibo Technologies <148161719+Bibo-Technologies@users.noreply.github.com>
2023-12-25 19:05:58 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -19,7 +19,7 @@ const database = getDatabase(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-/**/
+/*
    // Google Sign-In function
    function signInWithGoogle() {
         const provider = new GoogleAuthProvider();
@@ -72,7 +72,7 @@ const storage = getStorage(app);
     button.addEventListener("click", () => {
         signInWithGoogle();
     });
- 
+ */
     const toggle = document.getElementById('toggleForm');
     const formContainer = document.getElementById('formContainer');
     const backToSignInBtn = document.getElementById('backToSignIn');
@@ -154,7 +154,7 @@ signInForm.addEventListener('submit', async (e) => {
         }));
 
         // Redirect to the dashboard page upon successful sign-in
-        window.location.href = 'shop.html';
+        window.location.href = 'index.html';
     } catch (error) {
         // Handle errors, including reCAPTCHA verification failure
         const errorMessage = document.getElementById('error-message');
@@ -174,75 +174,175 @@ signInForm.addEventListener('submit', async (e) => {
    // return { success: true };
 //}
 
+// Initialize an array to store personal details
+let personalDetailsArray = [];
 
-        const signUpForm = document.getElementById('signUpForm');
-        const signUpBtn = document.getElementById('signUpBtn');
-        const spinner2 = document.querySelector('.spinner2');
-        const errorMessage2 = document.getElementById('error-message2');
-    
-        signUpForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const signUpFirstNameInput = document.getElementById('signUpFirstNameInput');
-            const signUpLastNameInput = document.getElementById('signUpLastNameInput');
-            const signUpTelephoneInput = document.getElementById('signUpTelephoneInput');
-            const signUpDOBInput = document.getElementById('signUpDOBInput');
-            const signUpEmailInput = document.getElementById('signUpEmailInput');
-            const signUpPasswordInput = document.getElementById('signUpPasswordInput');
-            const confirmPasswordInput = document.getElementById('confirmPasswordInput');
-            
-        
-            const firstName = signUpFirstNameInput.value;
-            const lastName = signUpLastNameInput.value;
-            const telephone = signUpTelephoneInput.value;
-            const dob = signUpDOBInput.value;
-            const email = signUpEmailInput.value;
-            const password = signUpPasswordInput.value;
-            const confirmPassword = confirmPasswordInput.value;
-            // Check if passwords match
-            if (password !== confirmPassword) {
-                errorMessage2.textContent = "Passwords do not match.";
-                return;
-            }
-        
-            try {
-                // Disable the button and show the loading spinner
-                signUpBtn.disabled = true;
-                signUpBtn.classList.add('loading-btn');
-                spinner2.style.display = 'block';
-        
-                // Create user with email and password
-                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-                const user = userCredential.user;
-        
-                // Send verification email
-                await sendVerificationEmail(user);
-        
-                // Save additional user data to Firebase Realtime Database
-                saveUser(user.uid, email,firstName,lastName,telephone,dob );
-        
-                // Display a success message
-                displaySuccessMessage();
-        
-                // Clear the form fields
-                signUpEmailInput.value = '';
-                signUpPasswordInput.value = '';
-                confirmPasswordInput.value = '';
-        
-                // Automatically click "Go Back to Sign In" button after a timeout
-                setTimeout(() => {
-                    backToSignInBtn.click();
-                }, 3000); // Adjust the timeout duration as needed
-            } catch (error) {
-                // Handle errors
-                errorMessage2.textContent = error.message;
-            } finally {
-                // Re-enable the button and hide the loading spinner
-                signUpBtn.disabled = false;
-                signUpBtn.classList.remove('loading-btn');
-                spinner2.style.display = 'none';
-            }
+// Get the input fields and the next button
+const firstNameInput = document.getElementById("signUpFirstNameInput");
+const lastNameInput = document.getElementById("signUpLastNameInput");
+const telephoneInput = document.getElementById("signUpTelephoneInput");
+const dobInput = document.getElementById("signUpDOBInput");
+const nextButton = document.getElementById("nextToAccountInfo");
+
+// Disable the Next button by default
+nextButton.disabled = true;
+
+// Add input event listeners to the fields
+firstNameInput.addEventListener("input", toggleNextButton);
+lastNameInput.addEventListener("input", toggleNextButton);
+telephoneInput.addEventListener("input", toggleNextButton);
+dobInput.addEventListener("input", toggleNextButton);
+
+// Add a click event listener to the Next button
+nextButton.addEventListener("click", storePersonalDetails);
+
+function toggleNextButton() {
+    // Check if all fields are filled before enabling the button
+    const allFieldsFilled =
+        firstNameInput.value.trim() !== "" &&
+        lastNameInput.value.trim() !== "" &&
+        telephoneInput.value.trim() !== "" &&
+        dobInput.value.trim() !== "";
+
+    // Update the disabled attribute of the button based on the condition
+    nextButton.disabled = !allFieldsFilled;
+}
+
+function storePersonalDetails() {
+    // Store the form details in the array with titles
+    personalDetailsArray = [
+        { title: "First Name", value: firstNameInput.value.trim() },
+        { title: "Last Name", value: lastNameInput.value.trim() },
+        { title: "Telephone", value: telephoneInput.value.trim() },
+        { title: "Date of Birth", value: dobInput.value.trim() },
+    ];
+
+    // Log the personal details to the console
+    console.log("Personal Details:", personalDetailsArray);
+
+    // Hide the personal information form and show the account information form
+    document.getElementById("personalInfoDiv").style.display = "none";
+    document.getElementById("accountInfoDiv").style.display = "block";
+}
+
+
+
+
+
+const signUpFormPersonal = document.getElementById('personalInfoForm');
+const signUpFormAccount = document.getElementById('accountInfoForm');
+const signUpBtn = document.getElementById('signUpBtn');
+const spinner2 = document.querySelector('.spinner2');
+const errorMessage2 = document.getElementById('error-message2');
+
+let personalFormData = {};
+let accountFormData = {};
+
+// Personal Information Form submission handling
+signUpFormPersonal.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Store the form details in the array with titles
+    personalDetailsArray = [
+        { title: "First Name", value: personalFormData.firstName },
+        { title: "Last Name", value: personalFormData.lastName },
+        { title: "Telephone", value: personalFormData.telephone },
+        { title: "Date of Birth", value: personalFormData.dob },
+    ];
+
+    // Log the personal details to the console
+    console.log("Personal Details:", personalDetailsArray);
+
+    // Hide the personal information form and show the account information form
+    document.getElementById("personalInfoDiv").style.display = "none";
+    document.getElementById("accountInfoDiv").style.display = "block";
+});
+
+// Account Information Form submission handling
+signUpFormAccount.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const signUpEmailInput = document.getElementById('signUpEmailInput');
+    const signUpPasswordInput = document.getElementById('signUpPasswordInput');
+    const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+
+    accountFormData = {
+        email: signUpEmailInput.value,
+        password: signUpPasswordInput.value,
+        confirmPassword: confirmPasswordInput.value,
+    };
+
+    // Check if passwords match
+    if (accountFormData.password !== accountFormData.confirmPassword) {
+        errorMessage2.textContent = "Passwords do not match.";
+        return;
+    }
+
+    try {
+        // Disable the button and show the loading spinner
+        signUpBtn.disabled = true;
+        signUpBtn.classList.add('loading-btn');
+        spinner2.style.display = 'block';
+
+        // Create user with email and password
+        const userCredential = await createUserWithEmailAndPassword(auth, accountFormData.email, accountFormData.password);
+        const user = userCredential.user;
+
+        // Send verification email
+        await sendVerificationEmail(user);
+
+        // Combine personal and account information
+        const combinedUserData = {
+            email: accountFormData.email,
+            firstName: personalDetailsArray.find(detail => detail.title === 'First Name')?.value,
+            lastName: personalDetailsArray.find(detail => detail.title === 'Last Name')?.value,
+            telephone: personalDetailsArray.find(detail => detail.title === 'Telephone')?.value,
+            dob: personalDetailsArray.find(detail => detail.title === 'Date of Birth')?.value,
+            // Add other fields as needed
+        };
+
+        // Save additional user data to Firebase Realtime Database
+        saveUser(user.uid, combinedUserData);
+        // Display a success message
+        displaySuccessMessage();
+
+        // Clear the form fields
+        signUpFirstNameInput.value = '';
+        signUpLastNameInput.value = '';
+        signUpTelephoneInput.value = '';
+        signUpDOBInput.value = '';
+        signUpEmailInput.value = '';
+        signUpPasswordInput.value = '';
+        confirmPasswordInput.value = '';
+
+      // Automatically reload the page after a timeout
+setTimeout(() => {
+    location.reload();
+}, 2000); // Adjust the timeout duration as needed
+
+    } catch (error) {
+        // Handle errors
+        errorMessage2.textContent = error.message;
+    } finally {
+        // Re-enable the button and hide the loading spinner
+        signUpBtn.disabled = false;
+        signUpBtn.classList.remove('loading-btn');
+        spinner2.style.display = 'none';
+    }
+});
+   // Modified saveUser function to handle the new structure
+function saveUser(userId, userData) {
+    const usersRef = ref(database, 'users/' + userId);
+
+    // Update the user data
+    update(usersRef, userData)
+        .then(() => {
+            console.log('User data saved successfully');
+        })
+        .catch((error) => {
+            console.error('Error saving user data:', error);
         });
-        
+}
         async function sendVerificationEmail(user) {
             // Send email verification
             await sendEmailVerification(user);
@@ -263,24 +363,10 @@ signInForm.addEventListener('submit', async (e) => {
                 document.body.removeChild(successMessage);
             }, 5000); // Adjust the timeout duration as needed
         }
-       
-        function saveUser(userId, email, firstName, lastName, telephone, dob) {
-            const usersRef = ref(database, 'users/' + userId);
+
         
-            update(usersRef, {
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                telephone: telephone,
-                dob: dob
-            })
-            .then(() => {
-                console.log('User data saved successfully');
-            })
-            .catch((error) => {
-                console.error('Error saving user data:', error);
-            });
-        }
+        
+        
         
 
         function displayMessage() {
