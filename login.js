fde84f0891b11c777377ae93ed8981aed77e18b0
Bibo Technologies <148161719+Bibo-Technologies@users.noreply.github.com>
2024-02-27 23:28:53 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -166,13 +166,25 @@ signInForm.addEventListener('submit', async (e) => {
         spinner.style.display = 'none';
     }
 });
-// Function to verify reCAPTCHA server-side (replace with your server-side logic)
-//async function verifyRecaptchaServerSide(response) {
-    // Implement server-side verification logic (consult reCAPTCHA documentation)
-    // Return an object with a 'success' property indicating verification status
-    // Example: { success: true } or { success: false, error: 'Verification failed' }
-   // return { success: true };
-//}
+
+
+async function verifyRecaptchaServerSide(token) {
+    try {
+        const response = await fetch('/verify-recaptcha', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ token })
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error verifying reCAPTCHA server-side:', error);
+        return { success: false, error: 'Server error' };
+    }
+}
+
 
 // Initialize an array to store personal details
 let personalDetailsArray = [];
